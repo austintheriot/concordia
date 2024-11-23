c6b8e10b8c732f42666c1f4eb680d6aa2f9ca18d
Austin Theriot <austinmtheriot@gmail.com>
2024-11-22 21:24:05 -0600

    configure static site generator and yarn workspace

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -1,21 +1,32 @@
-import { mdsvex } from "mdsvex";
-import adapter from '@sveltejs/adapter-auto';
+import { mdsvex } from 'mdsvex';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    // Consult https://svelte.dev/docs/kit/integrations
-    // for more information about preprocessors
-    preprocess: [vitePreprocess(), mdsvex()],
+  // Consult https://svelte.dev/docs/kit/integrations
+  // for more information about preprocessors
+  preprocess: [vitePreprocess(), mdsvex()],
 
-    kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	},
+  kit: {
+    // see https://svelte.dev/docs/kit/adapter-static
+    adapter: adapter({
+      // The directory to write prerendered pages to. It defaults to build.
+      pages: 'build',
+      // The directory to write static assets
+      assets: 'build',
+      // Specify a fallback page for SPA mode
+      fallback: undefined,
+      precompress: true,
+      strict: true
+    }),
+    // config for deploying to GitHub pages
+    paths: {
+      base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+    }
+  },
 
-    extensions: [".svelte", ".svx"]
+  extensions: ['.svelte', '.svx']
 };
 
 export default config;
