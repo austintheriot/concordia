8388aad684b6bba9a1c664a2f406152e1f17ac6b
Austin Theriot <austinmtheriot@gmail.com>
2024-11-22 21:01:01 -0600

    add static stite generator svelete adapter, test with build and preview

--- app/eslint.config.js
+++ app/eslint.config.js
@@ -1,4 +1,4 @@
-import prettier from "eslint-config-prettier";
+import prettier from 'eslint-config-prettier';
 import js from '@eslint/js';
 import svelte from 'eslint-plugin-svelte';
 import globals from 'globals';
@@ -7,27 +7,27 @@ import ts from 'typescript-eslint';
 export default ts.config(
   js.configs.recommended,
   ...ts.configs.recommended,
-  ...svelte.configs["flat/recommended"],
+  ...svelte.configs['flat/recommended'],
   prettier,
   ...svelte.configs['flat/prettier'],
   {
     languageOptions: {
-	  globals: {
-	    ...globals.browser,
-	    ...globals.node
-	  }
-	}
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    }
   },
   {
-    files: ["**/*.svelte"],
+    files: ['**/*.svelte'],
 
     languageOptions: {
-	  parserOptions: {
-	    parser: ts.parser
-	  }
-	}
+      parserOptions: {
+        parser: ts.parser
+      }
+    }
   },
   {
-    ignores: ["build/", ".svelte-kit/", "dist/"]
+    ignores: ['build/', '.svelte-kit/', 'dist/']
   }
 );
