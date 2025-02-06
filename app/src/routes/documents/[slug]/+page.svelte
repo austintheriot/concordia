bdeb2edc8a8f34c3315696170cb7bb771f88add0
Austin Theriot <austinmtheriot@gmail.com>
2025-02-05 20:05:55 -0600

    get a semblance of a popover showing up

--- app/src/routes/documents/[slug]/+page.svelte
+++ app/src/routes/documents/[slug]/+page.svelte
@@ -2,25 +2,50 @@
   import type { PageData } from './$types';
   import { detectWordUnderPointer } from '$lib/detectWordUnderPointer';
   import { replaceMacrons } from '$lib/replaceMacrons';
+  import { Popover } from 'bits-ui';
   const { data }: { data: PageData } = $props();
 
-  async function onPointerDown(e: PointerEvent): Promise<void> {
-    const result = detectWordUnderPointer(e);
-    if (!result || !result.word) {
-      return;
-    }
-    console.log(result.word);
-    const sanitizedWord = replaceMacrons(result.word).toLowerCase();
-    const res = await fetch(`/lemmas/${sanitizedWord}.txt`);
-    const lemma = await res.text();
+  let popoverOpen = $state(false);
+  let popoverInformation = $state('');
 
-    console.log(lemma);
+  async function onPointerDown(e: PointerEvent): Promise<void> {
+    try {
+      const detectedWordResult = detectWordUnderPointer(e);
+      if (!detectedWordResult || !detectedWordResult.word) {
+        return;
+      }
+      const sanitizedWord = replaceMacrons(detectedWordResult.word).toLowerCase();
+      const lemmaInformationRes = await fetch(`/lemmas/${sanitizedWord}.txt`);
+      if (lemmaInformationRes.ok) {
+        const lemmaInformation = await lemmaInformationRes.text();
+        popoverOpen = true;
+        popoverInformation = lemmaInformation;
+      } else {
+        popoverOpen = true;
+        popoverInformation = 'No lemma found';
+      }
+    } catch (e) {
+      console.error('Failed to fetch lemma', e);
+      popoverOpen = true;
+      popoverInformation = 'No lemma found';
+    }
   }
 </script>
 
 <div class="container" onpointerdown={onPointerDown}>
+  <Popover.Root bind:open={popoverOpen}>
+    <Popover.Trigger />
+    <Popover.Content class="popover-content">
+      <Popover.Close />
+      <Popover.Arrow />
+      {popoverInformation}
+    </Popover.Content>
+  </Popover.Root>
   <article>
     <!-- eslint-disable-next-line svelte/no-at-html-tags -->
     {@html data.post.html}
   </article>
 </div>
+
+<style>
+</style>
